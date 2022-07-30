af8d28a57fc48ca49ff16fecd2b5f3f9e18e2327
radosevic-dejan <radosevic.dejan@proton.me>
2022-07-30 12:52:36 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+
+function App() {
+  const [data, setData] = useState({});
+  const [location, setLocation] = useState("");
+
+  const image =
+    "https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_960_720.jpg";
+
+  const searchLocation = async (e) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
+
+    const response = await fetch(url);
+    const data = await response.json();
+    setData(data);
+
+    setLocation("");
+  };
+
+  const handleInput = (e) => {
+    setLocation(e.target.value);
+  };
+
+  return (
+    <>
+      <div className="container flex flex-col justify-between max-w-lg mx-auto mt-4">
+        <div className=" flex flex-col justify-center">
+          <input
+            className="text-md placeholder:placeholder-slate-500 px-2 py-4 border border-slate-600 rounded-lg"
+            type="text"
+            name="location"
+            value={location}
+            onChange={(e) => handleInput(e)}
+            placeholder="Enter location"
+          />
+          <button
+            className="w-[20%] mx-auto mt-3 px-4 py-2 border border-blue-500 rounded-xl"
+            onClick={(e) => searchLocation(e)}
+          >
+            Search
+          </button>
+        </div>
+
+        <div className=" flex justify-between py-2 px-2">
+          <div className="">
+            <p className="font-bold uppercase">{data.name}</p>
+            <p className="font-bold text-2xl">
+              {data.main ? <p>{data.main.temp}°C</p> : null}
+            </p>
+          </div>
+
+          <p className="rotate-45 font-bold capitalize">
+            {data.weather ? data.weather[0].main : null}
+          </p>
+        </div>
+
+        <div className="flex justify-around mt-4 bg-slate-100/90">
+          <p className="flex flex-col justify-center">
+            Feel
+            {data.main ? (
+              <span>
+                {data.main.feels_like}°C
+              </span>
+            ) : null}
+          </p>
+
+          <p className="flex flex-col justify-center">
+            Humidity
+            {data.main ? <span>{data.main.humidity}%</span> : null}
+          </p>
+
+          <p className="flex flex-col justify-center">
+            Wind
+            {data.wind ? (
+              <span>{(data.wind.speed / 0.514).toFixed(2)}m/s</span>
+            ) : null}
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
