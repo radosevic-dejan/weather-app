a9c69aa781e5cfeb7008e93908cf750deb84caad
radosevic-dejan <radosevic.dejan@proton.me>
2022-07-31 11:31:24 +0200

    Main functionality done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 
+const apiKey = import.meta.env.VITE_API_KEY
+
+
 function App() {
   const [data, setData] = useState({});
   const [location, setLocation] = useState("");
-
   const image =
     "https://cdn.pixabay.com/photo/2018/05/30/00/24/thunderstorm-3440450_960_720.jpg";
+
 
   const searchLocation = async (e) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
@@ -23,10 +26,18 @@ function App() {
 
   return (
     <>
-      <div className="container flex flex-col justify-between max-w-lg mx-auto mt-4">
-        <div className=" flex flex-col justify-center">
+      <div className="container flex flex-col justify-between max-w-lg mx-auto mt-4 relative">
+        <div className="w-full h-[700px] absolute bg-[#2e3b46]/50">
+          <img
+            src={`${image}`}
+            alt="Bg Image"
+            className="w-full h-full object-cover mix-blend-overlay"
+          />
+        </div>
+
+        <div className=" flex flex-col justify-center z-10 mt-4 px-2">
           <input
-            className="text-md placeholder:placeholder-slate-500 px-2 py-4 border border-slate-600 rounded-lg"
+            className="text-md capitalize placeholder:placeholder-slate-500 px-2 py-4 border border-slate-600 rounded-lg"
             type="text"
             name="location"
             value={location}
@@ -34,34 +45,30 @@ function App() {
             placeholder="Enter location"
           />
           <button
-            className="w-[20%] mx-auto mt-3 px-4 py-2 border border-blue-500 rounded-xl"
+            className="w-[20%] mx-auto mt-3 px-4 py-2 text-gray-500 font-bold bg-[#f87171] rounded-xl"
             onClick={(e) => searchLocation(e)}
           >
             Search
           </button>
         </div>
 
-        <div className=" flex justify-between py-2 px-2">
-          <div className="">
+        <div className="text-slate-300 flex justify-between mt-4 px-2 z-10">
+          <div className="py-4 px-2">
             <p className="font-bold uppercase">{data.name}</p>
             <p className="font-bold text-2xl">
-              {data.main ? <p>{data.main.temp}°C</p> : null}
+              {data.main ? <span>{data.main.temp}°C</span> : null}
             </p>
           </div>
 
-          <p className="rotate-45 font-bold capitalize">
+          <p className="rotate-45 font-bold capitalize mr-4">
             {data.weather ? data.weather[0].main : null}
           </p>
         </div>
 
-        <div className="flex justify-around mt-4 bg-slate-100/90">
+        <div className="flex justify-around mt-4 mx-2 py-4 rounded-lg bg-slate-100/90 z-10">
           <p className="flex flex-col justify-center">
             Feel
-            {data.main ? (
-              <span>
-                {data.main.feels_like}°C
-              </span>
-            ) : null}
+            {data.main ? <span>{data.main.feels_like}°C</span> : null}
           </p>
 
           <p className="flex flex-col justify-center">
